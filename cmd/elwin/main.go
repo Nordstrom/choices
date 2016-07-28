5f8ff38ad7b8fb43618441ea0952f87e935e01ac
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-07-28 10:28:11 -0700

    storage/mem: move ExampleData function into mem.

--- cmd/elwin/main.go
+++ cmd/elwin/main.go
@@ -30,7 +30,6 @@ import (
 
 	"github.com/foolusion/choices"
 	"github.com/foolusion/choices/elwin"
-	"github.com/foolusion/choices/storage/mem"
 	"github.com/foolusion/choices/storage/mongo"
 	"google.golang.org/grpc"
 )
@@ -54,44 +53,12 @@ var config = struct {
 	mongoCollection: "test",
 }
 
-func AddData() *mem.MemStore {
-	m := &mem.MemStore{}
-	t1 := choices.NewNamespace("t1", "test")
-	t1.AddExperiment(
-		"uniform",
-		[]choices.Param{{Name: "a", Value: &choices.Uniform{Choices: []string{"b", "c"}}}},
-		128,
-	)
-	m.AddNamespace(t1)
-	t2 := choices.NewNamespace("t2", "test")
-	t2.AddExperiment(
-		"weighted",
-		[]choices.Param{{Name: "b", Value: &choices.Weighted{Choices: []string{"on", "off"}, Weights: []float64{2, 1}}}},
-		128,
-	)
-	m.AddNamespace(t2)
-	t3 := choices.NewNamespace("t3", "test")
-	t3.AddExperiment(
-		"halfSegments",
-		[]choices.Param{{Name: "b", Value: &choices.Uniform{Choices: []string{"on"}}}},
-		64,
-	)
-	m.AddNamespace(t3)
-	t4 := choices.NewNamespace("t4", "test")
-	t4.AddExperiment(
-		"multi",
-		[]choices.Param{
-			{Name: "a", Value: &choices.Uniform{Choices: []string{"on", "off"}}},
-			{Name: "b", Value: &choices.Weighted{Choices: []string{"up", "down"}, Weights: []float64{1, 2}}},
-		},
-		128,
-	)
-	m.AddNamespace(t4)
-	return m
-}
-
 func main() {
 	log.Println("Starting elwin...")
+
+	// TODO: read environment variables for config
+
+	// create elwin config
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	ec, err := choices.NewElwin(
--- storage/mem/mem.go
+++ storage/mem/mem.go
@@ -74,3 +74,40 @@ func (m *MemStore) addns(n *choices.Namespace) {
 func (m *MemStore) AddNamespace(n *choices.Namespace) {
 	m.addns(n)
 }
+
+// ExampleData creates a MemStore with some example data loaded into it.
+func ExampleData() *MemStore {
+	m := &MemStore{}
+	t1 := choices.NewNamespace("t1", "test")
+	t1.AddExperiment(
+		"uniform",
+		[]choices.Param{{Name: "a", Value: &choices.Uniform{Choices: []string{"b", "c"}}}},
+		128,
+	)
+	m.addns(t1)
+	t2 := choices.NewNamespace("t2", "test")
+	t2.AddExperiment(
+		"weighted",
+		[]choices.Param{{Name: "b", Value: &choices.Weighted{Choices: []string{"on", "off"}, Weights: []float64{2, 1}}}},
+		128,
+	)
+	m.addns(t2)
+	t3 := choices.NewNamespace("t3", "test")
+	t3.AddExperiment(
+		"halfSegments",
+		[]choices.Param{{Name: "b", Value: &choices.Uniform{Choices: []string{"on"}}}},
+		64,
+	)
+	m.addns(t3)
+	t4 := choices.NewNamespace("t4", "test")
+	t4.AddExperiment(
+		"multi",
+		[]choices.Param{
+			{Name: "a", Value: &choices.Uniform{Choices: []string{"on", "off"}}},
+			{Name: "b", Value: &choices.Weighted{Choices: []string{"up", "down"}, Weights: []float64{1, 2}}},
+		},
+		128,
+	)
+	m.addns(t4)
+	return m
+}
