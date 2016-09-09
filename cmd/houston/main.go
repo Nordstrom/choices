c09b185932afb6dd4889cbda49ed06b05cc2c8de
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-09-09 16:54:22 -0700

    cmd/houston: get data to display

--- cmd/houston/main.go
+++ cmd/houston/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"strings"
 
+	"github.com/foolusion/choices"
 	"github.com/foolusion/choices/storage/mongo"
 
 	"gopkg.in/mgo.v2"
@@ -104,6 +106,76 @@ func main() {
 	}
 }
 
+// Namespace container for data from mongo.
+type Namespace struct {
+	Name        string
+	Labels      []string `bson:"teamid"`
+	Experiments []struct {
+		Name   string
+		Params []mongo.Param
+	}
+}
+
+// TableData container for data to be output.
+type TableData struct {
+	Name        string
+	Labels      string
+	Experiments []struct {
+		Name   string
+		Params []struct {
+			Name   string
+			Values string
+		}
+	}
+}
+
+type RootTmplData struct {
+	TestRaw []Namespace
+	ProdRaw []Namespace
+	Test    []TableData
+	Prod    []TableData
+}
+
+func namespaceToTableData(ns []Namespace) []TableData {
+	tableData := make([]TableData, len(ns))
+	for i, v := range ns {
+		tableData[i].Name = v.Name
+		tableData[i].Labels = strings.Join(v.Labels, ", ")
+		experiments := make(
+			[]struct {
+				Name   string
+				Params []struct {
+					Name   string
+					Values string
+				}
+			}, len(v.Experiments))
+		tableData[i].Experiments = experiments
+		for j, e := range v.Experiments {
+			tableData[i].Experiments[j].Name = e.Name
+			params := make(
+				[]struct {
+					Name   string
+					Values string
+				}, len(e.Params))
+			for k, p := range e.Params {
+				params[k].Name = p.Name
+				switch p.Type {
+				case choices.ValueTypeUniform:
+					var uniform choices.Uniform
+					p.Value.Unmarshal(&uniform)
+					params[k].Values = strings.Join(uniform.Choices, ", ")
+				case choices.ValueTypeWeighted:
+					var weighted choices.Weighted
+					p.Value.Unmarshal(&weighted)
+					params[k].Values = strings.Join(weighted.Choices, ", ")
+				}
+			}
+			tableData[i].Experiments[j].Params = params
+		}
+	}
+	return tableData
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	var buf []byte
 	var err error
@@ -113,12 +185,91 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Printf("%s", buf)
 
-	var result []mongo.MongoNamespace
-	cfg.mongo.DB(cfg.mongoDB).C(cfg.testCollection).Find(nil).All(&result)
+	var test []Namespace
+	cfg.mongo.DB(cfg.mongoDB).C(cfg.testCollection).Find(nil).All(&test)
+	var prod []Namespace
+	cfg.mongo.DB(cfg.mongoDB).C(cfg.prodCollection).Find(nil).All(&prod)
+
+	data := RootTmplData{
+		TestRaw: test,
+		ProdRaw: prod,
+		Test:    namespaceToTableData(test),
+		Prod:    namespaceToTableData(prod),
+	}
+
+	log.Println(data)
+
 	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintln(w, result)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := rootTemplate.Execute(w, data); err != nil {
+		log.Println(err)
+	}
 }
+
+var rootTemplate = template.Must(template.New("root").Parse(rootTmpl))
+
+const rootTmpl = `<!doctype html>
+<html lang="en">
+<head>
+<title>Houston!</title>
+</head>
+<body>
+<h1>Houston</h1>
+<div>
+{{with .Test}}
+<table>
+<tr>
+  <th>Namespace</th>
+  <th>Labels</th>
+  <th>Experiment</th>
+  <th>Params</th>
+  <th>Delete?</th>
+  <th>Launch?</th>
+</tr>
+{{range $ns := .}}
+{{range $exp := .Experiments}}
+<tr>
+	<th>{{$ns.Name}}</th>
+	<th>{{$ns.Labels}}</th>
+	<th>{{$exp.Name}}</th>
+	<th>{{range .Params}}<strong>{{.Name}}</strong>: ({{.Values}})<br/>{{end}}</th>
+	<th><a href="/delete/{{$exp.Name}}">Delete</a></th>
+	<th><a href="/launch/{{$exp.Name}}">Launch</a></th>
+</tr>
+{{end}}
+{{end}}
+</table>
+{{end}}
+
+{{with .Prod}}
+<table>
+<tr>
+  <th>Namespace</th>
+  <th>Labels</th>
+  <th>Experiment</th>
+  <th>Params</th>
+  <th>Delete?</th>
+  <th>Launch?</th>
+</tr>
+{{range $ns := .}}
+{{range $exp := .Experiments}}
+<tr>
+	<th>{{$ns.Name}}</th>
+	<th>{{$ns.Labels}}</th>
+	<th>{{$exp.Name}}</th>
+	<th>{{range .Params}}<strong>{{.Name}}</strong>: ({{.Values}})<br/>{{end}}</th>
+	<th><a href="/delete/{{$exp.Name}}">Delete</a></th>
+	<th><a href="/launch/{{$exp.Name}}">Launch</a></th>
+</tr>
+{{end}}
+{{end}}
+</table>
+{{end}}
+
+</div>
+</body>
+</html>
+`
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
