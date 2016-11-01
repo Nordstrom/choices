9cd9a9884fb43285f83cff05ecfad566460f7e01
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-10-31 17:09:14 -0700

    cmd/json-gateway: add a json gateway grpc/protobuf

---
+++ cmd/json-gateway/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+
+	gw "github.com/foolusion/choices/elwinstorage"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+)
+
+var (
+	storageEndpoint = flag.String("storage_endpoint", "elwin-storage:80", "endpoint of elwin-storage")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterElwinStorageHandlerFromEndpoint(ctx, mux, *storageEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8085", mux)
+}
+
+func main() {
+	flag.Parse()
+	log.Println("Starting json-gateway...")
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
