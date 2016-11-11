10ec923218f437e9b71d37380cd725bf0a97c894
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-11-11 10:42:40 -0800

    cmd/elwinator: param gets correct data for url.
    added the data needed to create the url for param pages.

--- cmd/elwinator/src/components/Param.js
+++ cmd/elwinator/src/components/Param.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 import { choiceNewURL } from '../urls';
 
-const Param = ({ namespaceName, experimentName, paramName }) => {
+const Param = ({ namespaceName, experimentName, p }) => {
   return (
     <div className="container">
-      <Link to={choiceNewURL(namespaceName, experimentName, paramName)}>Create new choice</Link>
+      <Link to={choiceNewURL(namespaceName, experimentName, p.name)}>Create new choice</Link>
     </div>
   );
 }
 
-export default Param;
+const mapStateToProps = (state, ownProps) => {
+  const ns = state.namespaces.find(n => n.name === ownProps.params.namespace);
+  const exp = ns.experiments.find(e => e.name === ownProps.params.experiment);
+  const p = exp.params.find(p => p.name === ownProps.params.param);
+  return {
+    namespaceName: ns.name,
+    experimentName: exp.name,
+    p,
+  }
+}
+
+const connected = connect(mapStateToProps)(Param);
+
+export default connected;
