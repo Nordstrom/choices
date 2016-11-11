aef8a14f141f8781a8bac79158553d3a112865c1
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-11-11 11:00:46 -0800

    cmd/elwinator: add a Namespace List to root page
    root page was pretty useless before

--- cmd/elwinator/src/App.js
+++ cmd/elwinator/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import NamespaceList from './components/NamespaceList';
+
 const App = ({ namespaceName, params, children }) => {
   return (
     <div className="container">
+      <h1>Namespaces</h1>
+      <NamespaceList />
       <Link to="/n/new">Create new namespace</Link>
     </div>
   );
