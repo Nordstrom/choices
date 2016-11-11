5f312fffe74bd71b27d912d3c60432c78987ab1e
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-11-11 11:08:37 -0800

    cmd/elwinator: fix url in App
    missed this url when i was updating them.

--- cmd/elwinator/src/App.js
+++ cmd/elwinator/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import NamespaceList from './components/NamespaceList';
+import { namespaceNewURL } from './urls';
 
 const App = ({ namespaceName, params, children }) => {
   return (
     <div className="container">
       <h1>Namespaces</h1>
       <NamespaceList />
-      <Link to="/n/new">Create new namespace</Link>
+      <Link to={namespaceNewURL()}>Create new namespace</Link>
     </div>
   );
 };
