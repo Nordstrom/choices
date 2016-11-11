6ea1986a84b1f23be661683cd55f271b1408ffca
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-11-11 10:44:12 -0800

    cmd/elwinator: add in bootstrap-theme css

--- cmd/elwinator/src/index.js
+++ cmd/elwinator/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import { Router,  Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
