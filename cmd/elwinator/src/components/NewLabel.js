1a053c165e70fa3644d4b5159bb8741b38e8cf86
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-11-11 10:43:42 -0800

    cmd/elwinator: add markup for new label component

--- cmd/elwinator/src/components/NewLabel.js
+++ cmd/elwinator/src/components/NewLabel.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const NewLabel = (props) => {
-  return <div className="container" />;
+  return (
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <label>Label</label>
+          <input type="text" className="form-control" />
+        </div>
+        <button type="submit" className="btn btn-primary" />
+      </form>
+    </div>
+  );
 }
 
 export default NewLabel;
