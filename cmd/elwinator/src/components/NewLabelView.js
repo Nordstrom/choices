766f68f4e6c9cd2dfcbe9d836c2f2684d908a75b
Andrew O'Neill <andrew.oneill@nordstrom.com>
2016-11-15 14:41:42 -0800

    cmd/elwinator: add NavSection to new label view
    forgot this in the last commit.

---
+++ cmd/elwinator/src/components/NewLabelView.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+import NavSection from './NavSection';
+import { rootURL, namespaceURL } from '../urls';
+import NewLabel from '../connectors/NewLabel';
+
+const NewLabelView = ({ params }) => {
+  return (
+    <div className="container">
+      <div className="row"><h1>Create a new experiment</h1></div>
+      <div className="row">
+        <NavSection>
+          <Link to={ rootURL() } className="nav-link">Home</Link>
+          <Link to={ namespaceURL(params.namespace) } className="nav-link">{params.namespace} - Namespace</Link>
+        </NavSection>
+        <div className="col-sm-9">
+          <NewLabel params={params}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NewLabelView;
