c2184a4777b9837da44a6cc89c47a34b9ca214b4
Andrew O'Neill <andrew.oneill@nordstrom.com>
2017-01-25 14:16:19 -0800

    cmd/elwin: update json response to include namespace
    json response will now include the namespace name so experiments names
    can be reused and differentiated more easily. also added some helper
    functions to clean up the json generation.

--- cmd/elwin/main.go
+++ cmd/elwin/main.go
@@ -279,6 +279,19 @@ func (ew *errWriter) write(buf []byte) {
 	_, ew.err = ew.w.Write(buf)
 }
 
+func (ew *errWriter) writeKey(buf []byte) {
+	ew.write(jsonQuote)
+	ew.write(buf)
+	ew.write(jsonQuote)
+	ew.write(jsonKeyEnd)
+}
+
+func (ew *errWriter) writeString(buf []byte) {
+	ew.write(jsonQuote)
+	ew.write(buf)
+	ew.write(jsonQuote)
+}
+
 var (
 	jsonQuote    = []byte{'"'}
 	jsonKeyEnd   = []byte{':', ' '}
@@ -290,30 +303,25 @@ var (
 func elwinJSON(er []choices.ExperimentResponse, w io.Writer) error {
 	ew := &errWriter{w: w}
 	ew.write(jsonOpenObj)
-	ew.write(jsonQuote)
-	ew.write([]byte("experiments"))
-	ew.write(jsonQuote)
-	ew.write(jsonKeyEnd)
+	ew.writeKey([]byte("experiments"))
 	ew.write(jsonOpenObj)
 	for i, exp := range er {
-		ew.write(jsonQuote)
-		ew.write([]byte(exp.Name))
-		ew.write(jsonQuote)
-		ew.write(jsonKeyEnd)
+		ew.writeKey([]byte(exp.Name))
+		ew.write(jsonOpenObj)
+		ew.writeKey([]byte("namespace"))
+		ew.writeString([]byte(exp.Namespace))
+		ew.write(jsonComma)
+		ew.writeKey([]byte("params"))
 		ew.write(jsonOpenObj)
 		for j, param := range exp.Params {
-			ew.write(jsonQuote)
-			ew.write([]byte(param.Name))
-			ew.write(jsonQuote)
-			ew.write(jsonKeyEnd)
-			ew.write(jsonQuote)
-			ew.write([]byte(param.Value))
-			ew.write(jsonQuote)
+			ew.writeKey([]byte(param.Name))
+			ew.writeString([]byte(param.Value))
 			if j < len(exp.Params)-1 {
 				ew.write(jsonComma)
 			}
 			paramCounts.With(prometheus.Labels{"exp": exp.Name, "param": param.Name, "value": param.Value}).Inc()
 		}
+		ew.write(jsonCloseObj)
 		ew.write(jsonCloseObj)
 		if i < len(er)-1 {
 			ew.write(jsonComma)
