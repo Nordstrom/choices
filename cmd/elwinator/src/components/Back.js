4a1fcf72d1e43f7b31ef26f8ed03da1d830c6e3f
David White <13scoobie@gmail.com>
2017-05-22 11:59:25 -0700

    delete elwinator

--- cmd/elwinator/src/components/Back.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { browserHistory } from 'react-router';
-
-const Back = props =>
-  <a className="nav-link" onClick={e => {
-    e.preventDefault(); browserHistory.goBack();
-  }}>Back</a>;
-
-export default Back
